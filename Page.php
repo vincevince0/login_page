c640978e60f3251de5e9b8cb90f465b8f8d95d62
vincevince0 <152151247+vincevince0@users.noreply.github.com>
2024-06-03 10:33:38 +0200

    Add files via upload

--- Page.php
+++ Page.php
@@ -9,167 +9,110 @@
 </head>
 <body>
 
-
 <?php 
-
-
 require 'vendor/autoload.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-function getNewToken()
-{
-    $token = password_hash(time(), PASSWORD_DEFAULT);
-    return $token;
+require_once('UserController.php');
+$usercontroller = new UserController();
+
+echo '<form method="post">
+        <button type="submit" class="loginForm-btn" name="loginForm-btn" id="loginForm-btn">Login Form</button>
+      </form>';
+
+if (isset($_POST['loginForm-btn'])) {
+    
+    echo '<table class="loginForm">';
+
+    echo '<tr><td><div class="loginText">Log In</div></td></tr>';
+
+    echo '<tr><td>';
+    echo '<input class="loginInput" type="email" placeholder="Email" required><br>';
+    echo '<input class="loginInput" type="password" placeholder="Password" required>';
+    echo '</tr></td>';
+
+    echo '<tr><td>';
+    echo '<form method="post">
+                <button type="submit" class="forgotPassword-btn" name="forgotPassword-btn" id="forgotPassword-btn">Forgot password</button>';
+    echo '      <button type="submit" class="registration-btn" name="registration-btn" id="registration-btn">Registration</button>
+          </form><br>';
+    echo '</td></tr>';
+
+    echo '<tr><td>';
+    echo '<form method="post">
+                <button type="submit" class="login-btn" name="login-btn" id="login-btn">Login</button>
+         </form>';
+    echo '</td></tr>';
+
+    echo'</table>';
 }
 
-
-
-    echo '<form method="post">
-            <button type="submit" class="loginForm-btn" name="loginForm-btn" id="loginForm-btn">Login Form</button>
+if(isset($_POST['forgotPassword-btn']))
+{
+    echo '<input class="loginInput" type="text" placeholder="Email" required><br>
+          <form method="post">
+                <button type="submit" class="finalizeRegistration-btn" name="newPass-btn" id="newPass-btn">Create New Password</button>
           </form>';
+}
 
-    if (isset($_POST['loginForm-btn'])) {
+if(isset($_POST['registration-btn']))
+{
+    echo '
+            <form method="post">
+            <input class="loginInput" type="text" id="name" name="name" placeholder="Name" required><br>
+            
+          <input class="loginInput" type="email" name="email" id="email" placeholder="Email" required><br>
+          <input class="loginInput" type="password" name="password1" id="password1" placeholder="Password" required><br>
+          <input class="loginInput" type="password" name="password2" id="password2" placeholder="Password again" required><br>
+          <button type="submit" class="finalizeRegistration-btn" name="finalizeRegistration-btn" id="finalizeRegistration-btn">Registration</button>
+          </form>';
+}
+
+if(isset($_POST['login-btn']))
+{
+    echo 'Logged in successfully!';
+}
+
+if (isset($_GET['token'])) {
+    $token = $_GET['token'];
+}
+
+if(isset($_POST['finalizeRegistration-btn']))
+{
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password1 = $_POST['password1'];
+    $password2 = $_POST['password2'];
+
+    if($password1 === $password2) {
+        $token = $usercontroller->getNewToken();
+        $created_at = date("Y-m-d H:i:s");
+        $usercontroller->newUser(0, 0, $name, $email, password_hash($password1, PASSWORD_DEFAULT), $token, date("Y-m-d H:i:s", time() + 3600), $created_at, date("Y-m-d H:i:s", time()), "none", "", "yes");
         
-        echo '<table class="loginForm">';
-
-        echo '<tr><td><div class="loginText">Log In</div></td></tr>';
-
-        echo '<tr><td>';
-        echo '<input class="loginInput" type="email" placeholder="Email" required><br>';
-        echo '<input class="loginInput" type="password" placeholder="Password" required>';
-        echo '</tr></td>';
-
-        echo '<tr><td>';
-        echo '<form method="post">
-                    <button type="submit" class="forgotPassword-btn" name="forgotPassword-btn" id="forgotPassword-btn">Forgot password</button>';
-        echo '      <button type="submit" class="registration-btn" name="registration-btn" id="registration-btn">Registration</button>
-              </form><br>';
-        echo '</td></tr>';
-
-        echo '<tr><td>';
-        echo '<form method="post">
-                    <button type="submit" class="login-btn" name="login-btn" id="login-btn">Login</button>
-             </form>';
-        echo '</td></tr>';
-
-        echo'</table>';
-    }
-
-    if(isset($_POST['forgotPassword-btn']))
-    {
-        echo '<input class="loginInput" type="text" placeholder="Email" required><br>
-              <form method="post">
-                    <button type="submit" class="finalizeRegistration-btn" name="newPass-btn" id="newPass-btn">Create New Password</button>
-              </form>';
-    }
-
-    if(isset($_POST['registration-btn']))
-    {
-        echo '
-                <form method="post">
-                <input class="loginInput" type="text" id="name" name="name" placeholder="Name" required><br>
-                
-              <input class="loginInput" type="email" name="email" id="email" placeholder="Email" required><br>
-              <input class="loginInput" type="password" name="password1" id="password1" placeholder="Password" required><br>
-              <input class="loginInput" type="password" name="password2" id="password2" placeholder="Password again" required><br>
-              <form method="post">
-                    <button type="submit" class="finalizeRegistration-btn" name="finalizeRegistration-btn" id="finalizeRegistration-btn">Registration</button>
-              </form>';
-    }
-
-    if(isset($_POST['login-btn']))
-    {
-        echo 'Logged in successfully!';
-    }
-
-    if(isset($_POST['finalizeRegistration-btn']))
-    {
         $mail = new PHPMailer();
-        $token = getNewToken();
- 
-            $mail->isSMTP();                                            
-            $mail->Host       = 'localhost';                    
-            $mail->SMTPAuth   = false;                                  
-            $mail->Port       = 1025;                                
- 
-            $mail->setFrom('from@example.com', 'Mailer');
-            $mail->addAddress($_POST['email'],$_POST['name']);
-       
-            $mail->isHTML(true);                                
-            $mail->Subject = 'Regisztráció';
-            $mail->Body    = 'A regisztráció véglegesítésének érdekében kattintson az alábbi linkre: <a href="http://localhost:8070/LoginPage/Page.php?token='.$token.'">';
-       
-            $mail->send();
-            //echo 'Message has been sent';
-    }
+        $mail->isSMTP();                                            
+        $mail->Host       = 'localhost';                    
+        $mail->SMTPAuth   = false;                                  
+        $mail->Port       = 1025;                                
 
-    function getRequest()
-    {
-        if (isset($_GET[token])) {
-            $token = $_GET[token];
-        }
+        $mail->setFrom('from@example.com', 'Mailer');
+        $mail->addAddress($email, $name);
+   
+        $mail->isHTML(true);                                
+        $mail->Subject = 'Registration';
+        $mail->Body    = 'To finalize your registration, please click on the following link: <a href="http://localhost:8070/LoginPage/Page.php?token='.$token.'">';
+   
+        $mail->send();
+        echo 'Registration complete! Check your email to finalize.';
+    } else {
+        echo 'Passwords do not match!';
     }
-
-    function keresEmail($email)
-    {
-        $query = "SELECT * FROM user WHERE email = '$email'";
- 
-        return $this->mysqli->query($query)->fetch_all(MYSQLI_ASSOC);
-    }
- 
-    function Registraion($email,$fullName,$password,$token)
-    {
-        if (empty($this->keresEmail($email))) {
-            $created_at = date("Y-m-d H:i:s");
-            $jelenlegi_ido = time();
-            $hozzaadando_ido = 3600;
-            $uj_ido = $jelenlegi_ido + $hozzaadando_ido;
- 
-            $jelzso = password_hash($password,PASSWORD_DEFAULT);
- 
-            $token_valid_until= date("Y-m-d H:i:s",$uj_ido);
-            $query = "INSERT INTO user (is_active,name,email,password,token,token_valid_until,created_at) VALUES ('0','$fullName','$email','$jelzso','$token','$token_valid_until','$created_at')";
-           
-            if ($this->mysqli->query($query) == TRUE) {
-                echo "Kattinson az emailben kapott linkre a regisztráció véglegesítéséért!";
-            }
-            else {
-                echo " ";
-            }
-        }
-        else {
-            echo 'Ezzel az email cím már létezik 1 profil!';
-        }
-    }
- 
-    function activateToken($token)
-    {
-       
-        $registered_at = date("Y-m-d H:i:s");
-        $token_valid_until = "SELECT token_valid_until FROM user WHERE token = '$token'";
-        if ($token_valid_until>=$registered_at) {
-            $query="UPDATE user SET is_active=1,registered_at='$registered_at' WHERE token ='$token' ";
- 
-            if ($this->mysqli->query($query) == TRUE) {
-                echo "Sikeres regisztráció!";
-            }
-            else {
-                echo "Hiba az adatok feltöltése közben.". $this->mysqli->error;
-            }
-        }
-        else{
-            echo"lejárt az idő!";
-        }
- 
-    }
+}
 
 ?>
 
-   
-
 </body>
 </html>
-
--- UserController.php
+++ UserController.php
@@ -1,5 +1,25 @@
 <?php
-function sendMail($email,$vezNev,$kerNev){
+
+class UserController
+{
+    private $mysqli;
+
+    public function __construct()
+    {
+        $this->mysqli = new mysqli("localhost", "root", "", "loginpage");
+        $this->mysqli->set_charset("utf8mb4");
+        if ($this->mysqli->connect_error) {
+            die("Connection failed: " . $this->mysqli->connect_error);
+        }
+    }
+
+    public function getNewToken()
+    {
+        $token = password_hash(time(), PASSWORD_DEFAULT);
+        return $token;
+    }   
+    public function sendMail($email,$vezNev,$kerNev)
+    {
         $mail = new PHPMailer();
         $token = $this->getNewToken();
  
@@ -25,4 +45,69 @@ function sendMail($email,$vezNev,$kerNev){
             echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
         }
     }
+
+    public function keresEmail($email)
+    {
+        $query = "SELECT * FROM user WHERE email = '$email'";
  
+        return $this->mysqli->query($query)->fetch_all(MYSQLI_ASSOC);
+    }
+ 
+    public function Registration($email,$fullName,$password,$token)
+    {
+        if (empty($this->keresEmail($email))) {
+            $created_at = date("Y-m-d H:i:s");
+            $jelenlegi_ido = time();
+            $hozzaadando_ido = 3600;
+            $uj_ido = $jelenlegi_ido + $hozzaadando_ido;
+ 
+            $jelzso = password_hash($password,PASSWORD_DEFAULT);
+ 
+            $token_valid_until= date("Y-m-d H:i:s",$uj_ido);
+            $query = "INSERT INTO user (is_active,name,email,password,token,token_valid_until,created_at) VALUES ('0','$fullName','$email','$jelzso','$token','$token_valid_until','$created_at')";
+           
+            if ($this->mysqli->query($query) == TRUE) {
+                echo "Kattinson az emailben kapott linkre a regisztráció véglegesítéséért!";
+            }
+            else {
+                echo " ";
+            }
+        }
+        else {
+            echo 'Ezzel az email cím már létezik 1 profil!';
+        }
+    }
+ 
+    public function activateToken($token)
+    {
+       
+        $registered_at = date("Y-m-d H:i:s");
+        $token_valid_until = "SELECT token_valid_until FROM user WHERE token = '$token'";
+        if ($token_valid_until>=$registered_at) {
+            $query="UPDATE user SET is_active=1,registered_at='$registered_at' WHERE token ='$token' ";
+ 
+            if ($this->mysqli->query($query) == TRUE) {
+                echo "Sikeres regisztráció!";
+            }
+            else {
+                echo "Hiba az adatok feltöltése közben.". $this->mysqli->error;
+            }
+        }
+        else{
+            echo"lejárt az idő!";
+        }
+ 
+    }
+
+    public function newUser($id, $is_active, $name, $email, $password, $token, $token_valid_until, $created_at, $registered_at, $picture, $deleted_at, $logged_in)
+{
+    $sql = "INSERT INTO `users` (id, is_active, name, email, password, token, token_valid_until, created_at, registered_at, picture, deleted_at, logged_in) 
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    $stmt = $this->mysqli->prepare($sql);
+    $stmt->bind_param("iissssssssss", $id, $is_active, $name, $email, $password, $token, $token_valid_until, $created_at, $registered_at, $picture, $deleted_at, $logged_in);
+    $stmt->execute();
+    $stmt->close();
+}
+
+
+}
